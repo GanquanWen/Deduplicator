e36aca7ad08e3ef050c724fa8b20f48898e5d518
GanquanWen <31709654+GanquanWen@users.noreply.github.com>
2018-04-06 16:24:28 -0400

    CLUI Prototype
    Prototype of command line user interface. Could tell if the command is valid.(Incorrect format and illegal commands)

---
+++ clui.py
@@ -0,0 +1,54 @@
+## Copyright 2018 Ganquan Wen wengq@bu.edu
+
+
+import sys
+
+def cli(cmd):
+	'''The format of input should be store -file [file] -locker [locker_location]
+	No space in the [file] and [locker_location] '''
+	command = cmd.split()
+	return command
+
+
+	
+def store(file, path):
+	return None
+
+
+def delete(file, path):
+	return None
+
+
+def retrieve(file, path):
+	return None
+
+
+def main():
+	while True:
+		'''read the command line from stdin and split it.
+		[0] is the command;
+		[2] is the name of the file;
+		[4] is the path to the locker.'''
+		command = sys.stdin.readline()
+		command = cli(command)
+		print command
+
+		'''Judge if the command is valid.'''
+		if command == ['exit']:
+			return None
+		elif len(command) != 5:
+			print 'Incorrect input format!'
+			continue
+		elif command[0] == 'store':
+			store(command[2],command[4])
+		elif command[0] == 'delete':
+			delete(command[2],command[4])
+		elif command[0] == 'retrieve':
+			retrieve(command[2],command[4])
+		else:
+			print 'Illegal command!(legal commands are: store; delete; retrieve.)'
+	return None
+
+
+if __name__ == '__main__':
+	main()
