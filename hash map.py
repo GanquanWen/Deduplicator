8766f428791d7a603b31f7635576ca0e37f4de97
XiangZheng <31711517+XiangZheng2017@users.noreply.github.com>
2018-04-06 17:21:42 -0400

    Update and rename hash map.ipynb to hash map.py

---
+++ hash map.py
@@ -0,0 +1,10 @@
+import hashlib
+
+def file_hash(filename):
+  h = hashlib.sha256()
+  with open(filename, 'rb', buffering=0) as f:
+    for b in iter(lambda : f.read(128*1024), b''):
+      h.update(b)
+  return h.hexdigest()
+
+file_hash('test.txt')
