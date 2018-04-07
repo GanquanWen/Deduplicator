b18faf6f363a73b2b7abbc15d9a2febc01d94d3d
XiangZheng <31711517+XiangZheng2017@users.noreply.github.com>
2018-04-07 15:55:21 -0400

    Update hash map.py

--- hash map.py
+++ hash map.py
@@ -1,10 +1,11 @@
 import hashlib
 
+#given a file name, hash it!
 def file_hash(filename):
   h = hashlib.sha256()
   with open(filename, 'rb', buffering=0) as f:
     for b in iter(lambda : f.read(128*1024), b''):
       h.update(b)
-  return h.hexdigest()
-
+    result=h.hexdigest()
+  return result
 file_hash('test.txt')
